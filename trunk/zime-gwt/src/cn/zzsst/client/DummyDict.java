876f590593ee14984f24cd39313343f634b51d30
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-07-29 04:29:28 +0000

    load dict from text file.

--- trunk/zime-gwt/src/cn/zzsst/client/DummyDict.java
+++ trunk/zime-gwt/src/cn/zzsst/client/DummyDict.java
@@ -3,51 +3,80 @@ package cn.zzsst.client;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+
 
 public class DummyDict implements Dict {
 
-	static protected HashMap<String, String> map;
-	
-	static {
-		map = new HashMap<String, String>();
-		map.put("zheo", "這");
-		map.put("na", "那");
-		map.put("bu", "不");
-		map.put("shy", "是");
-		map.put("di", "的");
-		map.put("le", "的");
-		map.put("ni", "你");
-		map.put("wo", "我");
-		map.put("ta", "他");
-		map.put("shang", "上");
-		map.put("zhung", "中");
-		map.put("xa", "下");
-		map.put("cian", "前");
-		map.put("hou", "後");
-		map.put("zuo", "左");
-		map.put("you", "右");
-	}
-	
-	public DummyDict(String codeName) {
-        // TODO
+    private HashMap<String, ArrayList<String>> index;
+    private String dictPath;
+
+    public DummyDict(String codeName) {
+        
+        index = new HashMap<String, ArrayList<String>>(); 
+        dictPath = "/schema/" + codeName + "/dict/dummy.txt";
+        
+        RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, dictPath);
+        try {
+            requestBuilder.sendRequest(null, new RequestCallback() {
+
+                @Override
+                public void onError(Request request, Throwable exception) {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                @Override
+                public void onResponseReceived(Request request, Response response) {
+                    loadIndex(response.getText());
+                }
+                
+            });
+        } catch (RequestException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    protected void loadIndex(String text) {
+        try {
+            String[] lines = text.trim().split("\n");
+            for (String line : lines) {
+                String[] r = line.split("[ \\t]+", 2);
+                final String key = r[0]; 
+                ArrayList<String> list;
+                if (index.containsKey(key)) {
+                    list = index.get(key);
+                } else {
+                    list = new ArrayList<String>();
+                    index.put(key, list);
+                }
+                list.add(r[1]);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 
     @Override
 	public ArrayList<String> lookup(Context context) {
-	    if (context.getCandidateLength() != 1)
-	        return null;
-		String s = map.get(context.getWord(0));
-		if (s == null)
-			return null;
-		ArrayList<String> list = new ArrayList<String>();
-		list.add(s);
-		return list;
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < context.getCandidateLength(); ++i) {
+            if (i > 0)
+                sb.append('-');
+	        sb.append(context.getWord(i));
+	    }
+		return index.get(sb.toString());
 	}
 
     public String parse(String preedit) {
         for (int i = preedit.length(); i > 0; --i) {
             final String s = preedit.substring(0, i);
-            if (map.containsKey(s))
+            if (index.containsKey(s))
                 return s;
         }
         return null;
