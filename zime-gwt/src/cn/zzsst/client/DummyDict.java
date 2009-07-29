e2083d7ae44127e95b43d16b8cfa1cce453a88fe
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-07-29 04:32:06 +0000

    relative dict path

--- zime-gwt/src/cn/zzsst/client/DummyDict.java
+++ zime-gwt/src/cn/zzsst/client/DummyDict.java
@@ -18,7 +18,7 @@ public class DummyDict implements Dict {
     public DummyDict(String codeName) {
         
         index = new HashMap<String, ArrayList<String>>(); 
-        dictPath = "/schema/" + codeName + "/dict/dummy.txt";
+        dictPath = "../schema/" + codeName + "/dict/dummy.txt";
         
         RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, dictPath);
         try {
