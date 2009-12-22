2fdb2b7d95eb2df5a65094e3febeb9f7a2c82ec1
zouivex <zouivex@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-12-22 10:54:31 +0000

    hacking...

--- plume/war/script/zimeimpl.js
+++ plume/war/script/zimeimpl.js
@@ -1,11 +1,19 @@
 // zimeimpl.js
 
 var RomanParser = Class.extend(Parser, {
+    // TODO
+    clear: function () {},
+    getPrompt: function () {},
+    processInput: function (event, ctx) {}
 });
 
 Parser.register("roman", RomanParser);
 
 var JSONFileBackend = Class.extend(Backend, {
+    // TODO
+    loadSchemaList: function (callback) {},
+    loadConfig: function (schemaName, callback) {},
+    query: function (input, callback) {}
 });
 
 Backend.register(JSONFileBackend);
