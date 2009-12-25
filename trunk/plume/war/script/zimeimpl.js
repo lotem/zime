1544851f4d3e8cd364c2c1fa8c49ccfca64bf30d
pan.sheen <pan.sheen@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-12-25 12:41:09 +0000

    GWTFrontend

--- trunk/plume/war/script/zimeimpl.js
+++ trunk/plume/war/script/zimeimpl.js
@@ -28,3 +28,43 @@ var JSONFileBackend = Class.extend(Backend, {
 });
 
 Backend.register(JSONFileBackend);
+
+var JSFrontend = Class.extend(Frontend, {
+    initialize: function () {
+        Logger.debug("JSFrontend.initialize");
+        this._backend = Backend.create();
+        this._schemaList = [];
+        var me = this;
+        this._backend.loadSchemaList(function (schemaList) {
+        	// debug code starts
+            var cont = $('<div id="schemaList" />');
+            cont.append($('<b>schemaList:</b><hr />'));
+            $.each(schemaList, function (i, item) {
+                var p = $('<p />').text (item.schema + " - " + item.displayName);
+                cont.append(p);
+            });
+            $("body").append(cont);
+            // debug code ends
+            me._schemaList = schemaList;
+            // TODO: call GWTFrontend.onSchemaListReady()
+            if (schemaList.length > 0) {
+                me.loadSchema(schemaList[0].schema);
+            }
+        });
+    },
+    loadSchema: function (schemaName) {
+        Logger.debug("JSFrontend.loadSchema: " + schemaName);
+        var me = this;
+        this._backend.loadConfig(schemaName, function (config) {
+            schema = new Schema (schemaName, config);
+            me.engine = new Engine(schema, me, me._backend);
+            // TODO: call GWTFrontend.onSchemaReady()
+        });
+    },
+    // updatePreedit: function (preeditText, selStart, selEnd) {},
+    // updateCandidates: function (candidateList) {}, 
+    // commit: function (commitText) {}, 
+    // submit: function () {}	
+});
+
+Frontend.register(JSFrontend);
