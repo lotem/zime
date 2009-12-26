58f258f31dcee896e2f5a5141d977c5fac2bd7f6
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-12-26 12:28:08 +0000

    make SchemaList a top level class.

---
+++ plume/src/zime/plume/client/impl/SchemaList.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package zime.plume.client.impl;
+
+import com.google.gwt.core.client.JsArray;
+
+public class SchemaList extends JsArray<SchemaListItem> {
+	protected SchemaList() {}
+}
