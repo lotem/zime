75236ace35c71be9bf6052a4250b0dd9bd4a71a0
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2009-12-26 05:52:16 +0000

    .

--- plume/src/zime/plume/client/Plume.java
+++ plume/src/zime/plume/client/Plume.java
@@ -1,9 +1,6 @@
 package zime.plume.client;
 
-import zime.plume.client.impl.RomanParserFactory;
-import zime.plume.client.impl.StaticFileBackend;
-import zime.plume.client.impl.WebFrontend;
-
+import zime.plume.client.impl.GWTFrontend;
 import com.google.gwt.core.client.EntryPoint;
 
 /**
@@ -13,10 +10,7 @@ public class Plume implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        // TODO: register parser factories
-        Parser.register(new RomanParserFactory());
-        // TODO: initiate front-end & back-end
-        new WebFrontend(new StaticFileBackend()).initialize();
+    	new GWTFrontend();
     }
 
 }
