89c538bb1e7f293b20196d27f324e1dc9e45edac
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-01-22 14:32:17 +0000

    jquery 1.4 seems causing problems in IE; reverted to jquery 1.3.2.

--- plume/distribute.sh
+++ plume/distribute.sh
@@ -17,7 +17,7 @@ ant build || exit -1
 mkdir war/data
 cp data/make-json.py war/data/
 mkdir war/script
-java -jar ~/opt/compiler.jar --js script/jquery-1.4.min.js --js script/jquery.json-2.2.min.js --js_output_file war/script/jquery.js --compilation_level WHITESPACE_ONLY
+java -jar ~/opt/compiler.jar --js script/jquery-1.3.2.min.js --js script/jquery.json-2.2.min.js --js_output_file war/script/jquery.js --compilation_level WHITESPACE_ONLY
 java -jar ~/opt/compiler.jar --js script/zimecore.js --js script/zimeimpl.js --js_output_file war/script/zime.js
 cp LICENSE.txt README.txt war/
 rm -R war/WEB-INF
