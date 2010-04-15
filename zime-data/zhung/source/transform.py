34969003751088dbd4edb35aa8fe05592fbece84
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-04-15 12:49:16 +0000

    zhengzhou-hua

--- zime-data/zhung/source/transform.py
+++ zime-data/zhung/source/transform.py
@@ -4,9 +4,9 @@ import re
 import fileinput
 
 config = '''
-gi-j !zi-j !v-w bu zhi zr-zh !zr-z !rh-r !zh-z !l-n
+gi-j !zi-j v-w bu zhi zr-zh !zr-z !rh-r !zh-z !l-n
 ueng-ung ien-ian uon-uan m-n !ng-n rhy-el o-eo ptk-h 
-!5-tones !4-tones 2-tones !1-tone
+!5-tones 4-tones !2-tones !1-tone
 '''.split()
 
 RULES = {
@@ -24,7 +24,7 @@ RULES = {
     ],
     'v-w' : [
         (r'^v([aoeu])', r'w\1'),
-        (r'^vi', r'wui'),
+        #(r'^vi', r'wui'),
     ],
     'bu' : [
         (r'^([bpmfv])un(\w*)$', r'\1en\2'),
