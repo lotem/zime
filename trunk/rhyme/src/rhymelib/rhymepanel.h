be313294e0f9e685945d20fe5389048143d5f781
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-04-19 02:03:18 +0000

    rhyme client source files; Code::Blocks projects.

---
+++ trunk/rhyme/src/rhymelib/rhymepanel.h
@@ -0,0 +1,14 @@
+#ifndef RHYMEPANEL_H
+#define RHYMEPANEL_H
+
+
+class RhymePanel
+{
+    public:
+        RhymePanel();
+        virtual ~RhymePanel();
+    protected:
+    private:
+};
+
+#endif // RHYMEPANEL_H
