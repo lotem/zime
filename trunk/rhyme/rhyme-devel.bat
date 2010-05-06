b9e0319da62d15bc1a47af94196a5c54e16aad47
chen.sst <chen.sst@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-05-06 14:03:15 +0000

    commit local changes before moving to site B.

---
+++ trunk/rhyme/rhyme-devel.bat
@@ -0,0 +1,4 @@
+set PATH=%PATH%;C:\Python26;\apps\MinGW\bin
+set HOME=\home
+cd \home\devel\rhyme\engine
+cmd
--- trunk/rhyme/src/rhymelib/rhymeclient.h
+++ trunk/rhyme/src/rhymelib/rhymeclient.h
@@ -1,12 +1,22 @@
 #ifndef RHYMECLIENT_H
 #define RHYMECLIENT_H
 
+#include <string>
+
+struct RhymeMessage
+{
+    bool result;
+};
 
 class RhymeClient
 {
     public:
         RhymeClient();
         virtual ~RhymeClient();
+        bool checkService();
+        void startSession(std::wstring const& schema);
+        void endSession();
+        RhymeMessage processKeyEvent(long keyCode, long mask);
     protected:
     private:
 };
