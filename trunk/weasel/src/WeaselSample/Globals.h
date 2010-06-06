90852aa22015b5b9dba1c31ba95092c92f82e3cb
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-06-06 06:50:15 +0000

    added TSF sample.

---
+++ trunk/weasel/src/WeaselSample/Globals.h
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////////////////
+//
+//  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+//  ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED
+//  TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+//
+//  Copyright (C) 2003  Microsoft Corporation.  All rights reserved.
+//
+//  Globals.h
+//
+//          Global variable declarations.
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <windows.h>
+#include <ole2.h>
+#include <olectl.h>
+#include <assert.h>
+#include "msctf.h"
+
+void DllAddRef();
+void DllRelease();
+
+//#define ARRAYSIZE(a) (sizeof(a)/sizeof(a[0]))
+
+#define TEXTSERVICE_LANGID    MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)
+
+#define TEXTSERVICE_DESC    L"中州韻輸入法引擎"
+#define TEXTSERVICE_DESC_A   "ZIME"
+#define TEXTSERVICE_MODEL   TEXT("Apartment")
+
+#define TEXTSERVICE_ICON_INDEX  0
+#define LANGBAR_ITEM_DESC   L"ZIME Button"
+
+//+---------------------------------------------------------------------------
+//
+// SafeStringCopy
+//
+// Copies a string from one buffer to another.  wcsncpy does not always
+// null-terminate the destination buffer; this function does.
+//----------------------------------------------------------------------------
+
+inline void SafeStringCopy(WCHAR *pchDst, ULONG cchMax, const WCHAR *pchSrc)
+{
+    if (cchMax > 0)
+    {
+        wcsncpy(pchDst, pchSrc, cchMax);
+        pchDst[cchMax-1] = '\0';
+    }
+}
+
+extern HINSTANCE g_hInst;
+
+extern LONG g_cRefDll;
+
+extern CRITICAL_SECTION g_cs;
+
+extern const CLSID c_clsidTextService;
+
+extern const GUID c_guidProfile;
+
+extern const GUID c_guidLangBarItemButton;
+
+extern const GUID c_guidDisplayAttributeInput;
+
+#endif // GLOBALS_H
---
+++ trunk/weasel/src/WeaselSample/resource.h
@@ -0,0 +1,6 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TextService.rc
+//
+#define IDI_ZH		                    100
+#define IDI_ALPHA                       101
