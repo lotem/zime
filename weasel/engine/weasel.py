f39d506d9af56f853ea14130f3ff2329cd020f57
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-08-14 02:15:24 +0000

    implement a straightforward weasel service in engine/weasel.py.

--- weasel/engine/weasel.py
+++ weasel/engine/weasel.py
@@ -2,15 +2,9 @@
 # -*- coding: utf-8 -*-
 # vim:set et sts=4 sw=4:
 
-import os
-import SocketServer
-from ctypes import *
-
-ERROR_ALREADY_EXISTS = 183
-EVENT_ALL_ACCESS = 0x1F0003
-
 import logging
 import logging.config
+import os
 
 logging.config.fileConfig("logging.conf")
 logger = logging.getLogger("weasel")
@@ -26,9 +20,9 @@ from zimedb import DB
 def _initialize():
     zimeparser.register_parsers()
     # initialize DB 
-    HOME_PATH = os.getenv('HOME')
-    if HOME_PATH:
-        db_path = os.path.join(HOME_PATH, '.ibus', 'zime')
+    home_path = os.getenv('HOME') or os.getenv('USERPROFILE')
+    if home_path:
+        db_path = os.path.join(home_path, '.ibus', 'zime')
     else:
         db_path = '.'
     user_db = os.path.join(db_path, 'zime.db')
@@ -39,40 +33,95 @@ def _initialize():
 
 _initialize()
 
-class WeaselSessionHandler(SocketServer.StreamRequestHandler):
 
-    def handle(self):
-        for line in self.rfile:
-            tag, params = line.rstrip("\n").split("=", 1)
-            if tag == "INIT":
-                logger.info("init session: %s", params)
-                self.__lookup_table = ibus.LookupTable()
-                self.__engine = zimeengine.SchemaChooser(self, params)
-            if tag == "EVENT":
-                keycode, mask = map(int, params.split(","))
-                logger.debug("process_key_event: '%s'(%x), %08x" % (keysyms.keycode_to_name(keycode), keycode, mask))
-                ret = self.__engine.process_key_event(keycode, mask)
-                self.wfile.write("RET=%s\n" % ("true" if ret else "false"))
-                self.wfile.flush()
+class Session:
+
+    def __init__(self, params=''):
+        logger.info("init session: %s", params)
+        self.__lookup_table = ibus.LookupTable()
+        self.__engine = zimeengine.SchemaChooser(self, params)
+        self.__clear()
+
+    def __clear(self):
+        self.__commit = None
+        self.__preedit = None
+        self.__aux = None
+        self.__cand = None
+
+    def process_key_event(self, keycode, mask):
+        logger.debug("process_key_event: '%s'(%x), %08x" % (keysyms.keycode_to_name(keycode), keycode, mask))
+        self.__clear()
+        taken = self.__engine.process_key_event(keycode, mask)
+        if taken:
+            return self.__reply()
+        else:
+            return None
+
+    def __reply(self):
+        action = set()
+        r = list()
+        if self.__commit:
+            action.add(u'commit')
+            r.append(u'commit=%s\n' % u''.join(self.__commit)) 
+        if self.__preedit:
+            action.add(u'update_ctx')
+            (s, attrs) = self.__preedit
+            r.append(u'preedit=%s\n' % s)
+            if attrs:
+                r.append(u'preedit.attr.length=%d\n' % len(attrs))
+                for i in range(len(attrs)):
+                    (extent, type) = attrs[i]
+                    r.append(u'preedit.attr.%d.range=%d,%d\n' % (i, extent[0], extent[1]))
+                    r.append(u'preedit.attr.%d.type=%s\n' % (i, type))
+        if self.__cand:
+            action.add(u'update_ctx')
+            (current_page, total_pages, highlighted, cands) = self.__cand
+            r.append(u'cand.current_page=%d\n' % current_page)
+            r.append(u'cand.total_pages=%d\n' % total_pages)
+            r.append(u'cand.highlighted=%d\n' % highlighted)
+            r.append(u'cand.length=%d\n' % len(cands))
+            for i in range(len(cands)):
+                r.append(u'cand.%d=%s\n' % (i, cands[i][0]))
+        if self.__aux:
+            action.add(u'update_ctx')
+            (s, attrs) = self.__aux
+            r.append(u'aux=%s\n' % s)
+        #self.__clear()
+        if not action:
+            return u'action=noop\n'
+        else:
+            r.insert(0, u'action=%s\n' % u','.join(sorted(action)))
+            return u''.join(r)
+    
+    # implement a frontend proxy for zimeengine
 
     def commit_string(self, s):
         logger.debug(u'commit: [%s]' % s)
-        self.wfile.write("COMMIT=%s\n" % s.encode('utf-8'))
+        if self.__commit:
+            self.__commit.append(s)
+        else:
+            self.__commit = [s]
 
     def update_preedit(self, s, start, end):
         logger.debug(u'preedit: [%s[%s]%s]' % (s[:start], s[start:end], s[end:]))
-        self.wfile.write("PREEDIT=%s\n" % s.encode('utf-8'))
-        self.wfile.write("CURSOR=%d,%d\n" % (start, end))
+        if s:
+            attrs = [((start, end), u'HIGHLIGHTED')] if start < end else None
+            self.__preedit = (s, attrs)
+        else: 
+            self.__preedit = None
 
     def update_aux_string(self, s):
         logger.debug(u'aux: [%s]' % s)
-        self.wfile.write("AUX=%s\n" % s.encode('utf-8'))
+        if s:
+            self.__aux = (s, None)
+        else:
+            self.__aux = None
 
     def update_candidates(self, candidates):
         self.__lookup_table.clean()
         self.__lookup_table.show_cursor(False)
         if not candidates:
-            self.wfile.write("CANDIDATES=0\n")
+            self.__cand = None
         else:
             for c in candidates:
                 self.__lookup_table.append_candidate(ibus.Text(c[0]))
@@ -84,10 +133,9 @@ class WeaselSessionHandler(SocketServer.StreamRequestHandler):
         c = self.__lookup_table.get_cursor_pos()
         p = self.__lookup_table.get_page_size()
         current_page = c / p
-        total_number_of_pages = (n + p - 1) / p
-        self.wfile.write("CANDIDATES=%d,%d,%d\n" % (len(candidates), current_page, total_number_of_pages))
-        for x in candidates:
-            self.wfile.write(x.get_text().encode('utf-8') + "\n")
+        total_pages = (n + p - 1) / p
+        cands = [(x.get_text(), None) for x in candidates]
+        self.__cand = (current_page, total_pages, c % p, cands)
             
     def page_up(self):
         if self.__lookup_table.page_up():
@@ -127,43 +175,42 @@ class WeaselSessionHandler(SocketServer.StreamRequestHandler):
         #print u'candidate_cursor_pos = %d' % index
         return index
 
+
 class WeaselService:
     
-    HOST, PORT = "localhost", 2133
-    MUTEX_NAME = "WeaselService"
-    EVENT_NAME = "WeaselServiceReadyEvent"
+    def __init__(self):
+        self.__sessions = dict()
 
-    def run(self):
-        if not self.check_single_instance():
-            return
-        self.service_ready_notify()
-        addr = (WeaselService.HOST, WeaselService.PORT)
-        self.server = SocketServer.TCPServer(addr, WeaselSessionHandler)
-        logger.info("WeaselService running...")
-        self.server.serve_forever()
+    def cleanup(self):
+        self.__sessions.clear()
 
-    def check_single_instance(self):
-        mutex = windll.kernel32.CreateMutexA(None, True, WeaselService.MUTEX_NAME)
-        if not mutex:
-            logger.error("error creating mutex '%s'; exiting." % WeaselService.MUTEX_NAME)
+    def has_session(self, id):
+        if id in self.__sessions:
+            return True
+        else:
             return False
-        if windll.kernel32.GetLastError() == ERROR_ALREADY_EXISTS:
-            logger.info("existing WeaselService found; exiting.")
-            windll.kernel32.CloseHandle(mutex)
+
+    def get_session(self, id):
+        if id in self.__sessions:
+            return self.__sessions[id]
+        else:
+            return None
+
+    def create_session(self):
+        try:
+            session = Session()
+        except Exception, e:
+            logger.error("create_session: error creating session: %s" % e)
+            return None
+        self.__sessions[id(session)] = session
+        return id(session)
+
+    def destroy_session(self, id):
+        if id not in self.__sessions:
+            logger.warning("destroy_session: invalid session id %d." % id)
             return False
+        del self.__sessions[id]
         return True
 
-    def service_ready_notify(self):
-        event = windll.kernel32.OpenEventA(EVENT_ALL_ACCESS, False, WeaselService.EVENT_NAME)
-        if not event:
-            logger.warning("error opening event '%s'." % WeaselService.EVENT_NAME)
-            return
-        windll.kernel32.SetEvent(event)
-        windll.kernel32.CloseHandle(event)
+service = WeaselService()
 
-def main():
-    service = WeaselService()
-    service.run()
-
-if __name__ == "__main__":
-    main()
