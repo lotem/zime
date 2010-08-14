9fff452f69edb638a386b7ad5ed77537ec311d29
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-08-14 02:15:24 +0000

    implement a straightforward weasel service in engine/weasel.py.

--- branches/weasel-devel/weasel/weasel-devel.bat
+++ branches/weasel-devel/weasel/weasel-devel.bat
@@ -1,4 +1,2 @@
-set PATH=%PATH%;C:\Python27;D:\apps\devel\MinGW\bin;D:\home\devel\boost_1_43_0
-set HOME=D:\home\devel\weasel
-cd /d %HOME%
+set PATH=%PATH%;C:\Python27;D:\apps\devel;D:\apps\devel\MinGW\bin;D:\home\devel\boost_1_43_0
 cmd
