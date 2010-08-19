5697be021eb1cc180ff44210f6695ca444f951c9
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-08-19 19:33:11 +0000

    disable logging in release.

--- weasel/data/populate-db.bat
+++ weasel/data/populate-db.bat
@@ -1,3 +1,7 @@
 call ..\env.bat
 python create-schema.py -v Pinyin.txt
+python create-schema.py -v -k ComboPinyin.txt
+python create-schema.py -v -k DoublePinyin.txt
+python make-phrases.py jyutping
+python create-schema.py -v Jyutping.txt
 pause
--- weasel/release.bat
+++ weasel/release.bat
@@ -6,9 +6,9 @@ copy release\WeaselServer.exe output
 copy lib\boost_python-vc90-mt-1_43.dll output
 mkdir output\engine
 copy engine\*.py output\engine
-copy engine\*.conf output\engine
 mkdir output\engine\ibus
 copy engine\ibus\*.py output\engine\ibus
+copy misc\*.conf output\engine
 copy misc\install.bat output
 copy misc\uninstall.bat output
 copy misc\stop_service.bat output
