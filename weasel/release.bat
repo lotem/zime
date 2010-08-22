45e390ed39f8005edf721a57ade7d44bdc383092
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-08-22 11:47:52 +0000

    增VC9运行时库

--- weasel/release.bat
+++ weasel/release.bat
@@ -1,14 +1,27 @@
 call env.bat
-del /q output\*
-rmdir /s /q output\engine
-copy release\weasel.ime output
-copy release\WeaselServer.exe output
-copy lib\boost_python-vc90-mt-1_43.dll output
-mkdir output\engine
-copy engine\*.py output\engine
-mkdir output\engine\ibus
-copy engine\ibus\*.py output\engine\ibus
-copy misc\*.conf output\engine
-copy misc\*.js output
-copy misc\*.bat output
+set OUTPUT=output\weasel
+
+mkdir %OUTPUT%
+del /q %OUTPUT%\*
+rmdir /s /q %OUTPUT%\data
+rmdir /s /q %OUTPUT%\engine
+rmdir /s /q %OUTPUT%\Microsoft.VC90.CRT
+copy release\weasel.ime %OUTPUT%
+copy release\WeaselServer.exe %OUTPUT%
+copy lib\boost_python-vc90-mt-1_43.dll %OUTPUT%
+mkdir %OUTPUT%\data
+copy data\*.bat %OUTPUT%\data
+copy data\*.py %OUTPUT%\data
+copy data\*.txt %OUTPUT%\data
+mkdir %OUTPUT%\engine
+copy engine\*.py %OUTPUT%\engine
+mkdir %OUTPUT%\engine\ibus
+copy engine\ibus\*.py %OUTPUT%\engine\ibus
+copy misc\*.bat %OUTPUT%
+copy misc\*.conf %OUTPUT%\engine
+copy misc\*.js %OUTPUT%
+copy misc\*.txt %OUTPUT%
+mkdir %OUTPUT%\Microsoft.VC90.CRT
+copy misc\Microsoft.VC90.CRT\* %OUTPUT%\Microsoft.VC90.CRT
+
 pause
