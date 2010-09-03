815feed90223a4741d808b1344203ffa5f820fab
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-09-03 17:48:37 +0000

    detect os version.

--- branches/weasel-devel/weasel/release.bat
+++ branches/weasel-devel/weasel/release.bat
@@ -13,6 +13,7 @@ copy engine\ibus\*.py %OUTPUT%\engine\ibus
 copy misc\*.bat %OUTPUT%
 copy misc\*.conf %OUTPUT%\engine
 copy misc\*.js %OUTPUT%
+copy misc\*.py %OUTPUT%
 copy misc\*.txt %OUTPUT%
 copy release\weasel.ime %OUTPUT%
 copy release\WeaselServer.exe %OUTPUT%
