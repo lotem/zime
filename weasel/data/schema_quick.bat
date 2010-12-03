a3d27e63da43e1be0e350f8f8ade9b1ec0a76932
chen.sst@gmail.com <chen.sst@gmail.com@9a988479-703f-0410-81cf-1fc39a31c57a>
2010-12-03 09:34:56 +0000

    removed data, engine.

--- weasel/data/schema_quick.bat
+++
@@ -1,8 +0,0 @@
-@call ..\env.bat
-@echo installing schema Quick.
-
-..\WeaselServer.exe /q
-python make-phrases.py quick
-python zimedb-admin.py -vi Quick.txt
-
-@pause
--- weasel/data/schema_tonal_pinyin.bat
+++
@@ -1,8 +0,0 @@
-@call ..\env.bat
-@echo installing schema TonalPinyin.
-
-..\WeaselServer.exe /q
-python make-phrases.py tonal-pinyin
-python zimedb-admin.py -vi TonalPinyin.txt
-
-@pause
